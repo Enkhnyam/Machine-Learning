9e95287df22c47cbbf8507c5ca1a3290dd357945
Enkhnyam <110773025+Enkhnyam@users.noreply.github.com>
2023-02-17 17:36:46 +0100

    data preprocessing template for the file

--- linearRegression/simpleLinearRegression.py
+++ linearRegression/simpleLinearRegression.py
@@ -1,1 +1,24 @@
+# Importing the libraries
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+
+dataSet = pd.read_csv('Salary_Data.csv')
+x = dataSet.iloc[:, :-1].values
+y = dataSet.iloc[:, -1].values
+
+# Importing the dataset into the Training set and Test set
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1/3, random_state=0)
+
+# Training the Simple Linear Regression model on the Training set
+
+# Predicting the Test set results
+
+# Visualising the Training set results
+
+# Visualising the Test set results
