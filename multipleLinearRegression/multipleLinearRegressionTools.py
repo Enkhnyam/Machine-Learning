3b90dd50e504657ddf51060079afeb94cf1220e5
Enkhnyam <110773025+Enkhnyam@users.noreply.github.com>
2023-02-18 15:17:11 +0100

    Tools that could be useful for linear regression but with multiple features

---
+++ multipleLinearRegression/multipleLinearRegressionTools.py
@@ -0,0 +1,36 @@
+# Importing the libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+
+dataSet = pd.read_csv('50_Startups.csv')
+x = dataSet.iloc[:, :-1].values
+y = dataSet.iloc[:, -1].values
+
+# Encoding categorical data
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+# Splitting the dataset into the Training set and Test set
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+# Training the Multiple Linear Regression model on the Training set
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+# Predicting the Test set results
+
+y_pred = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+structured_data = np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1)
