a024d095f2a3f33d4928d2a336a16698a391419e
Enkhnyam <110773025+Enkhnyam@users.noreply.github.com>
2023-02-18 12:14:03 +0100

    Very simple linear Regression

--- linearRegression/simpleLinearRegression.py
+++ linearRegression/simpleLinearRegression.py
@@ -13,12 +13,32 @@ y = dataSet.iloc[:, -1].values
 # Importing the dataset into the Training set and Test set
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1/3, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-# Training the Simple Linear Regression model on the Training set
+# Training the Simple Linear Regression model on the Training set 
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
 
 # Predicting the Test set results
 
+y_pred = regressor.predict(x_test)
+
 # Visualising the Training set results
 
+plt.scatter(x_train, y_train, color='red')
+plt.plot(x_train, regressor.predict(x_train), color='blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
 # Visualising the Test set results
+
+plt.scatter(x_test, y_test, color='red')
+plt.plot(x_train, regressor.predict(x_train), color='blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
