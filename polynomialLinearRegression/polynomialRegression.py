a64e7bfdc1d1b8d643baca91049761bff68fc116
Enkhnyam <110773025+Enkhnyam@users.noreply.github.com>
2023-02-19 18:10:11 +0100

    PolynomialRegression
    Here I used the Position_Salary.csv to predict how much money someone would probably earn with Position Level of 6.5

--- polynomialLinearRegression/polynomialRegression.py
+++ polynomialLinearRegression/polynomialRegression.py
@@ -1,1 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(x, y)
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree=4)
+x_poly = poly_reg.fit_transform(x)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(x_poly, y)
+
+plt.scatter(x, y, color='red')
+plt.plot(x, lin_reg.predict(x), color='blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+plt.scatter(x, y, color='m')
+plt.plot(x, lin_reg_2.predict(x_poly), color='blue')
+plt.title('Truth or Bluff (Polynomial Regression')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+X_grid = np.arange(min(x), max(x), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(x, y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+lin_reg.predict([[6.5]])
+
+lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
+
 
