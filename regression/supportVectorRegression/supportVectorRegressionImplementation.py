44a3fefd244eb944babf4979740479c0c29ac1cd
Enkhnyam <110773025+Enkhnyam@users.noreply.github.com>
2023-02-20 15:45:39 +0100

    supportVectorRegressionToolsCode

--- regression/supportVectorRegression/supportVectorRegressionImplementation.py
+++ regression/supportVectorRegression/supportVectorRegressionImplementation.py
@@ -1,1 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataSet = pd.read_csv('Position_Salaries (1).csv')
+x = dataSet.iloc[:, 1:-1].values
+y = dataSet.iloc[:, -1].values
+
+print(x)
+print(y)
+y = y.reshape(len(y), 1)
+print(y)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+sc_y = StandardScaler()
+x = sc_x.fit_transform(x)
+y = sc_y.fit_transform(y)
+print(x)
+print(y)
+
+# Training the SVR model on the whole dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel = 'rbf')
+regressor.fit(x, y)
+
+# Predicting a new result
+sc_y.inverse_transform(regressor.predict(sc_x.transform([[6.5]])).reshape(-1, 1))
+
+# Visualising the SVR results
+plt.scatter(sc_x.inverse_transform(x), sc_y.inverse_transform(y), color='red')
+plt.plot(sc_x.inverse_transform(x), sc_y.inverse_transform(regressor.predict(x).reshape(-1, 1)))
+plt.title('SVR')
+plt.xlabel('Position level')
+plt.ylabel('Salary($m)')
+plt.show()
+
+# Visualising the SVR results (for higher resolution and smoother curve)
+X_grid = np.arange(min(sc_x.inverse_transform(x)), max(sc_x.inverse_transform(x)), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(sc_x.inverse_transform(x), sc_y.inverse_transform(y), color = 'red')
+plt.plot(X_grid, sc_y.inverse_transform(regressor.predict(sc_x.transform(X_grid)).reshape(-1, 1)), color = 'blue')
+plt.title('SVR')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
 
