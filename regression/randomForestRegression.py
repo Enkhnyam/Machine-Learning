90803bfce1f6d797e63222bdcef1d9206fcd60d3
Enkhnyam <110773025+Enkhnyam@users.noreply.github.com>
2023-02-22 15:10:09 +0100

    randomForestRegressionModelTools

---
+++ regression/randomForestRegression.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(x, y)
+
+regressor.predict([[6.5]])
+
+X_grid = np.arange(min(x), max(x), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(x, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Random Forest Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
